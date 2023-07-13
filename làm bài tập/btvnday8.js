8c611ef2caff97f3175113b3393ca6be0ab328a2
HieuLe3097 <134619683+HieuLe3097@users.noreply.github.com>
2023-07-13 22:22:31 +0700

    Delete làm bài tập directory

--- làm bài tập/btvnday8.js
+++
@@ -1,9 +0,0 @@
-// const month = prompt('Enter number')
-// console.log('tháng', month)
-
-const text = prompt('Max 10 character')
-for(let i = 0; i <= 10;)
-if (i <= 10) {console.log('go')
-} else { console.log('text')
-}
-
