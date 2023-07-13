7f8acec5184e1589690f1e2fa83a88db3ac7e074
HieuLe3097 <134619683+HieuLe3097@users.noreply.github.com>
2023-07-13 23:33:30 +0700

    bai2.js

--- Bài thi cuối kì 1/bai2.js
+++ Bài thi cuối kì 1/bai2.js
@@ -1,5 +1,13 @@
-let star = "*";
-for (let i = 0; i < 10; i++) {
-  console.log(star);
-  star = star + "*";
+function numberOneTriangle() {
+  const n = prompt("Nhập một số từ 1 đến 10:");
+
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
 }
+
+numberOneTriangle();
